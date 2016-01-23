e19c33425850fb395205d5a4cb6af6ca89424062
Jared Ririe <jaredririe20@gmail.com>
2016-01-23 10:42:12 -0700

    Followed ROS tutorials to create a catkin workspace (stored in catkin_ws folder). Wrote simple listener/subscriber python scripts.

---
+++ catkin_ws/build/publisher_subscriber/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ecestudent/ecen490/catkin_ws/src/publisher_subscriber
+# Build directory: /home/ecestudent/ecen490/catkin_ws/build/publisher_subscriber
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ catkin_ws/build/publisher_subscriber/catkin_generated/package.cmake
@@ -0,0 +1,7 @@
+set(_CATKIN_CURRENT_PACKAGE "publisher_subscriber")
+set(publisher_subscriber_MAINTAINER "ecestudent <ecestudent@todo.todo>")
+set(publisher_subscriber_DEPRECATED "")
+set(publisher_subscriber_VERSION "0.0.1")
+set(publisher_subscriber_BUILD_DEPENDS "roscpp" "rospy" "std_msgs")
+set(publisher_subscriber_RUN_DEPENDS "roscpp" "rospy" "std_msgs")
+set(publisher_subscriber_BUILDTOOL_DEPENDS "catkin")
