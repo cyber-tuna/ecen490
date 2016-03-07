701f44db705a5bac45cb9a2dfc2014aa44bf891e
Jared Ririe <jaredririe20@gmail.com>
2016-03-08 06:39:32 +1030

    Added rush_goal and go_to_point_behind_ball functions

--- zeromq/subscriber/sub_client2.py
+++ zeromq/subscriber/sub_client2.py
@@ -41,7 +41,6 @@ def receive(threadName, *args):
 
     while True:
         address, contents = subscriber.recv_multipart()
-        print contents
 
         contentArray = contents.split()
 
@@ -70,11 +69,30 @@ def receive(threadName, *args):
         try:
             mutex.release()
         except:
-            print "Attempting to release unlocked mutex"
+            pass
 
     # We never get here but clean up anyhow
     subscriber.close()
     context.term()
+
+
+def rush_goal():
+    mutex.acquire()
+
+    desiredPoint = param.HOME_GOAL
+
+    while (home_robot_state.pos_x_est < (desiredPoint.x - 0.1)) or \
+            (home_robot_state.pos_y_est > (desiredPoint.y + 0.1) or home_robot_state.pos_y_est < (desiredPoint.y - 0.1)):
+
+        command = MotionSkills.go_to_point(home_robot_state, desiredPoint)
+        angular_command = MotionSkills.go_to_angle(home_robot_state, param.HOME_GOAL)
+        omega = angular_command.omega
+
+
+        velchange.goXYOmegaTheta(command.vel_x, command.vel_y, omega, home_robot_state.pos_theta_est)
+        time.sleep(0.01)
+
+    velchange.goXYOmega(0,0,0)
 
 
 def go_to_point_behind_ball():
@@ -84,110 +102,54 @@ def go_to_point_behind_ball():
     ball = Point.Point(ball_x, ball_y)
     desiredPoint = MotionSkills.getPointBehindBall(ball)
 
-    while (home_robot_state.pos_x_est > (desiredPoint.x + 0.05) or home_robot_state.pos_x_est < (desiredPoint.x - 0.05)) and /
+    robot_point = Point.Point(home_robot_state.pos_x_est, home_robot_state.pos_y_est)
+    desiredAngle = MotionSkills.angleBetweenPoints(robot_point, param.HOME_GOAL)
+
+    print "angle", param.radianToDegree(home_robot_state.pos_theta_est)
+    print "desiredAngle", param.radianToDegree(desiredAngle)
+
+    while (home_robot_state.pos_x_est > (desiredPoint.x + 0.05) or home_robot_state.pos_x_est < (desiredPoint.x - 0.05)) or \
             (home_robot_state.pos_y_est > (desiredPoint.y + 0.05) or home_robot_state.pos_y_est < (desiredPoint.y - 0.05)):
 
-        command = MotionSkills.go_to_point(home_robot_wstate, desiredPoint)
-        velchange.goXYOmegaTheta(command.vel_x, command.vel_y, 0, home_robot_state.pos_theta_est)
+        command = MotionSkills.go_to_point(home_robot_state, desiredPoint)
+        angular_command = MotionSkills.go_to_angle(home_robot_state, param.HOME_GOAL)
+        omega = angular_command.omega
+
+
+        velchange.goXYOmegaTheta(command.vel_x, command.vel_y, omega , home_robot_state.pos_theta_est)
         time.sleep(0.01)
 
+
+    angular_command = MotionSkills.go_to_angle(home_robot_state, param.HOME_GOAL)
+    velchange.goXYOmega(0,0,angular_command.omega)
+    time.sleep(angular_command.runTime)
+
+    velchange.goXYOmega(0,0,0)
+
+def go_to_center():
+    mutex.acquire()
+
+    desiredPoint = param.CENTER
+
+    while (home_robot_state.pos_x_est > (desiredPoint.x + 0.05) or home_robot_state.pos_x_est < (desiredPoint.x - 0.05)) or \
+            (home_robot_state.pos_y_est > (desiredPoint.y + 0.05) or home_robot_state.pos_y_est < (desiredPoint.y - 0.05)):
+
+        command = MotionSkills.go_to_point(home_robot_state, desiredPoint)
+        angular_command = MotionSkills.go_to_angle(home_robot_state, param.HOME_GOAL)
+        omega = angular_command.omega
+
+        velchange.goXYOmegaTheta(command.vel_x, command.vel_y, omega , home_robot_state.pos_theta_est)
+        time.sleep(0.01)
 
     velchange.goXYOmega(0,0,0)
 
 
 def score_goal():
-    print "Scoring Goal"
-
     mutex.acquire()
 
-    #Update the robot state
-    # while robot_x == "NULL" or robot_y == "NULL" or robot_angle == "NULL" or ball_x == "NULL" or ball_y == "NULL":
-    #     print "NULL state values detected"
-
-    state.pos_x_est = param.pixelToMeter(float(robot_x))
-    state.pos_y_est = param.pixelToMeter(float(robot_y))
-    state.pos_theta_est = param.degreeToRadian(float(robot_angle))
-
-    ball = Point.Point(param.pixelToMeter(float(ball_x)), param.pixelToMeter(float(ball_y)))
-
-    # print("Ball x: %s Ball y: %s" % (ball.x, ball.y))
-    desiredPoint = MotionSkills.getPointBehindBall(ball)
-    # print("Point behind ball x: %f y: %f" % (desiredPoint.x, desiredPoint.y))
-
-    command = MotionSkills.go_to_point(state, desiredPoint)
-    velchange.goXYOmega(command.vel_x, command.vel_y, 0)
-    time.sleep(command.runTime)
-    velchange.goXYOmega(0,0,0)
-
-    #Update the robot state
-    while robot_x == "NULL" or robot_y == "NULL" or robot_angle == "NULL" or ball_x == "NULL" or ball_y == "NULL":
-        print "NULL state values detected"
-
-    state.pos_x_est = param.pixelToMeter(float(robot_x))
-    state.pos_y_est = param.pixelToMeter(float(robot_y))
-    state.pos_theta_est = param.degreeToRadian(float(robot_angle))
-
-    command = MotionSkills.go_to_angle(state, param.HOME_GOAL)
-    velchange.goXYOmega(0, 0, command.omega)
-    time.sleep(command.runTime)
-    velchange.goXYOmega(0,0,0)
-
-    while robot_x == "NULL" or robot_y == "NULL" or robot_angle == "NULL" or ball_x == "NULL" or ball_y == "NULL":
-        print "NULL state values detected"
-
-    state.pos_x_est = param.pixelToMeter(float(robot_x))
-    state.pos_y_est = param.pixelToMeter(float(robot_y))
-    state.pos_theta_est = param.degreeToRadian(float(robot_angle))
-    #
-    currentPoint = Point.Point(state.pos_x_est, state.pos_y_est)
-    distance = MotionSkills.disBetweenPoints(currentPoint, param.HOME_GOAL)
-    length = math.sqrt((param.HOME_GOAL.x-state.pos_x_est)**2+(param.HOME_GOAL.y-state.pos_y_est)**2)
-    runTime = length/0.5
-    vel_x = 0.5
-    velchange.goXYOmega(vel_x,0,0)
-    time.sleep(runTime)
-    velchange.goXYOmega(0,0,0)
-
-    print "Finished score_goal"
-
-def defend_goal():
-    while True:
-        if robot_x == "NULL" or robot_y == "NULL" or robot_angle == "NULL" or ball_x == "NULL" or ball_y == "NULL":
-            velchange.goXYOmega(0,0,0)
-            continue
-
-        #Update the robot state
-        state.pos_x_est = param.pixelToMeter(float(robot_x))
-        state.pos_y_est = param.pixelToMeter(float(robot_y))
-        state.pos_theta_est = param.degreeToRadian(float(robot_angle));
-
-        if robot_x == "NULL" or robot_y == "NULL" or robot_angle == "NULL" or ball_x == "NULL" or ball_y == "NULL":
-            velchange.goXYOmega(0,0,0)
-            continue
-        if (state.pos_y_est > param.pixelToMeter(float(ball_y)) - 0.1) and (state.pos_y_est < param.pixelToMeter(float(ball_y)) + 0.1):
-            velchange.goXYOmega(0,0,0)
-
-            if abs(MotionSkills.deltaBetweenAngles(state.pos_theta_est,0)) > param.degreeToRadian(float(3)):
-                print abs(MotionSkills.deltaBetweenAngles(state.pos_theta_est,0))
-                zeroAngle = Point.Point(param.pixelToMeter(350), param.pixelToMeter(float(robot_y)))
-                command = MotionSkills.go_to_angle(state, zeroAngle)
-                velchange.goXYOmega(0, 0, command.omega)
-                time.sleep(command.runTime)
-                velchange.goXYOmega(0,0,0)
-        else:
-            #desiredPoint = Point.Point(param.pixelToMeter(float(ball_x)), param.pixelToMeter(float(ball_y)))
-
-            if float(ball_y) < 85 and float(ball_y) > -75:
-                if state.pos_y_est > param.pixelToMeter(float(ball_y)):
-                    vel_x = -0.7
-                else:
-                    vel_x = 0.7
-                velchange.goXYOmega(0,vel_x,0)
-            else:
-                velchange.goXYOmega(0,0,0)
-
-        #time.sleep(command.runTime)
-        #velchange.goXYOmega(0,0,0)
+    go_to_point_behind_ball()
+    # rush_goal()
+    # go_to_center()
 
 
 def main():
@@ -213,8 +175,9 @@ def main():
     #     print "invalid skill"
     #     return
 
-    defend_goal()
-    # score_goal()
+    #score_goal()
+    #rush_goal()
+    go_to_center()
 
 if __name__ == "__main__":
     print "Calibrating Roboclaws"
